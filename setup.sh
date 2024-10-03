994a4ff97b6dee9d059581444e785d5fa41c6af6
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2024-10-04 03:13:46 +0530

    Added faiss and enchant to setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=
 python3 -m pip install "cython<3.0.0" "pyyaml==5.3.1"
 python3 -m pip install -r requirements.txt
 conda install -c conda-forge libsndfile==1.0.31
+conda install enchant pyenchant
+conda install pytorch::faiss-gpu
