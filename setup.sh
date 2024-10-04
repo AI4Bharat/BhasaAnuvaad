0d353d3edffe41184a4b1eab59302b4a258f30ca
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2024-10-04 13:46:27 +0530

    Added -y to ensure autodownload

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
-python3 -m pip install "cython<3.0.0" "pyyaml==5.3.1"
+conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia -y
+python3 -m pip install "cython<3.0.0" "pyyaml==5.3.1" -y
 python3 -m pip install -r requirements.txt
-conda install -c conda-forge libsndfile==1.0.31
-conda install enchant pyenchant
-conda install pytorch::faiss-gpu
+conda install -c conda-forge libsndfile==1.0.31 -y
+conda install enchant pyenchant -y
+conda install pytorch::faiss-gpu -y
