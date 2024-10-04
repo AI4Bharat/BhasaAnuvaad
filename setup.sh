260ef013c185fb8a0b1716a72b71201ba2c68d07
Nikhil Narasimhan <nikhil.narasimhan99@gmail.com>
2024-10-04 14:02:46 +0530

    Fixed installation of enchant

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=
 python3 -m pip install "cython<3.0.0" "pyyaml==5.3.1" -y
 python3 -m pip install -r requirements.txt
 conda install -c conda-forge libsndfile==1.0.31 -y
-conda install enchant pyenchant -y
+conda install conda-forge::pyenchant conda-forge::enchant -y
 conda install pytorch::faiss-gpu -y
